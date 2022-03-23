dd8e03c907d722c7abb8f0cfc8aa5fbb9b6ad19e
James Bowers <jamesbowers123@gmail.com>
2022-03-23 15:30:36 +0000

    refactor / dry-up plane & port test suites with beforeEach

--- __test__/airport.test.js
+++ __test__/airport.test.js
@@ -3,29 +3,30 @@ const Aeroplane = require('../src/plane.js');
 const Flightplan = require('../src/flightplan.js');
 
 describe('airport', () => {
+    describe('with airports and airplanes', () => {
+        let airport;
+        
+        beforeEach(() => {
+            airport = new Airport('Sitges');
+            flightplan = {
+                airports: [airport]
+            };
+            ba149 = new Aeroplane(flightplan);
+            ba127 = new Aeroplane(flightplan);
+    });
+
     it('can be instantiated', () => {
         expect(new Airport()).toBeInstanceOf(Object);
     });
     it('has a name', () => {
-        const airport = new Airport('Sitges');
-
         expect(airport.airport).toEqual('Sitges');
     });
     it('can add planes', () => {
-        const airport = new Airport('Sitges');
-        const flightplan = new Flightplan([airport]);
-        const ba149 = new Aeroplane(flightplan);
-        
         airport.addPlane(ba149);
         expect(airport.aeroplanes).toContain(ba149);
         expect(ba149.currentAirport).toBe(airport);
     });
     it('can remove planes', () => {
-        const airport = new Airport('Sitges');
-        const flightplan = new Flightplan([airport])
-        const ba149 = new Aeroplane(flightplan);
-        const ba127 = new Aeroplane(flightplan);
-
         airport.addPlane(ba127);
         airport.addPlane(ba149);
         airport.removePlane(ba149);
@@ -33,5 +34,11 @@ describe('airport', () => {
         expect(airport.aeroplanes).toContain(ba127);
         expect(ba127.currentAirport).toBe(airport);
         expect(ba149.currentAirport).toBeFalsy;
-    })
-})
+    });
+
+
+});
+    
+    
+    
+});
--- __test__/plane.test.js
+++ __test__/plane.test.js
@@ -3,49 +3,50 @@ const Aeroplane = require('../src/plane.js');
 const Flightplan = require('../src/flightplan.js');
 
 describe('Aeroplane', () => {
-    it('instantiates an object', () => {
-        const sitges = new Airport('Sitges')
-        const granCan = new Airport('Gran Canaria');
+    describe('with airports and a flightplan', () => {
+        let sitges;
+        let granCan;
 
-        const flightplan = new Flightplan([sitges, granCan]);
-        const aeroplane = new Aeroplane(flightplan);
-        
-        expect(aeroplane).toBeInstanceOf(Object);
+        beforeEach(() => {
+            sitges = {
+                addPlane: jest.fn(),
+                removePlane: jest.fn(),
+                name: 'Sitges',
+                planes: []
+            };
+            granCan = {
+                addPlane: jest.fn(),
+                removePlane: jest.fn(),
+                name: 'Gran Canaria',
+                planes: []
+            };
+            flightplan = {
+                airports: [sitges, granCan]
+            };
+            aeroplane = new Aeroplane(flightplan);
+        });
+
+        it('instantiates an object', () => {                  
+            expect(aeroplane).toBeInstanceOf(Object);
+        });
+        it('has a starting port', () => { 
+            expect(aeroplane.currentAirport).toEqual(sitges);
+        });
+        it('can take off', () => {    
+            aeroplane.takeOff();
+    
+            expect(aeroplane.currentAirport).toBeFalsy();
+            expect(aeroplane.previousAirport).toEqual(flightplan.airports[0]);
+            expect(aeroplane.previousAirport.planes).toStrictEqual([]);       
+        });
+        it('can land at a different airport', () => {    
+            aeroplane.takeOff();
+            aeroplane.land();
+    
+            expect(aeroplane.currentAirport).toEqual(granCan);
+            expect(aeroplane.previousAirport).toEqual(sitges);
+        });
+    
     });
-    it('has a starting port', () => {
-        const sitges = new Airport('Sitges')
-        const granCan = new Airport('Gran Canaria');
-
-        const flightplan = new Flightplan([sitges, granCan]);
-        const aeroplane = new Aeroplane(flightplan);
-
-        expect(aeroplane.currentAirport).toEqual(sitges);
-    });
-    it('can take off', () => {
-        const sitges = new Airport('Sitges')
-        const granCan = new Airport('Gran Canaria');
-
-        const flightplan = new Flightplan([sitges, granCan]);
-        const aeroplane = new Aeroplane(flightplan);
-
-        aeroplane.takeOff();
-
-        expect(aeroplane.currentAirport).toBeFalsy();
-        expect(aeroplane.previousAirport).toEqual(flightplan.airports[0]);
-        expect(aeroplane.previousAirport.planes).toBeFalsy();       
-    });
-    it('can land at a different airport', () => {
-        const sitges = new Airport('Sitges');
-        const granCan = new Airport('Gran Canaria');
-
-        const flightplan = new Flightplan([sitges, granCan]);
-        const aeroplane = new Aeroplane(flightplan);
-
-        aeroplane.takeOff();
-        aeroplane.land();
-
-        expect(aeroplane.currentAirport).toEqual(granCan);
-        expect(aeroplane.previousAirport).toEqual(sitges);
-    });
-
+    
 });
